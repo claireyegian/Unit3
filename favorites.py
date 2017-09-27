104cf1d9c31f905f4b340a845d3521ce7d5d840e
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-27 13:49:19 -0400

    Updated from Brython Server: 9/27/2017 1:49:19 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Claire Yegian
+#9/27/17
+#favorites.py - prints favorite word favorite number of times
 
+word = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+
+for i in range (0,num):
+    print(word)
