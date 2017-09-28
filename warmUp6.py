807cc8a4aebf00ab99ae0583c7235b5848788652
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-28 14:19:01 -0400

    Updated from Brython Server: 9/28/2017 2:19:02 PM

--- warmUp6.py
+++ warmUp6.py
@@ -2,5 +2,10 @@
 #9/28/17
 #warmUp6.py - counts to 10 mississippi
 
-for i in range(0,10):
+for i in range(1,11):
     print(i, 'mississippi')
+
+i=1
+while i<=10:
+    print(i, 'mississippi')
+    i += 1
