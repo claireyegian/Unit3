99af5b002f68344bf7cbe50a417dd5e59cb94f08
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-28 14:14:40 -0400

    Updated from Brython Server: 9/28/2017 2:14:41 PM

--- breakDemo.py
+++ breakDemo.py
@@ -1,3 +1,10 @@
 #Claire Yegian
 #9/28/17
 #breakDemo.py - how to exit a loop early
+
+#copy cat
+while True:
+    text = input('Say something: ')
+    print(text)
+    if text == 'something':
+        break
