ae60deff8f5fca5b47933a9d72e1145b8e88ff7a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-28 14:47:17 -0400

    Updated from Brython Server: 9/28/2017 2:47:17 PM

--- gauss.py
+++ gauss.py
@@ -2,9 +2,19 @@
 #9/28/17
 #gauss.py - adds numbers
 
+"""
 i=100
 total = 0
 while i>=0:
     total = total + i
+    i = i-1
+print(total)"""
+
+num1 = int(input('Enter the begining of the range: '))
+num2 = int(input('Enter the end of the range: '))
+i = num2
+total = 0
+while i>=num1:
+    total = total+i
     i = i-1
 print(total)
