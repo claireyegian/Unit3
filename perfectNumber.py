d58a19a3c6fb5f6fefd2fdedbc50cea02fd3b000
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-29 16:56:42 -0400

    Updated from Brython Server: 9/29/2017 4:56:41 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,11 @@
-p
+#Claire Yegian
+#9/29/17
+#perfectNumber.py - finds if number is perfect
+
+num = int(input('Enter a number: '))
+i = 1
+while i<num:
+    if num%i == 0:
+        div = i
+        total = div
+        
