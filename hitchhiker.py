6718f12087f070ec5984d54a01b262915285e8b7
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-29 14:32:29 -0400

    Updated from Brython Server: 9/29/2017 2:32:28 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,10 @@
+#Claire Yegian
+#9/29/17
+#hitchhiker.py - answers question with '42'
+
+while True:
+    question = input('Ask a question or type "quit": ')
+    print(42)
+    if question == 'quit':
+        break
 
