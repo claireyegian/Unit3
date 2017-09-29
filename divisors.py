5eb69bbb2204151ee8d470a89050b1019d91c03d
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-29 16:49:50 -0400

    Updated from Brython Server: 9/29/2017 4:49:49 PM

--- divisors.py
+++ divisors.py
@@ -1,1 +1,10 @@
+#Claire Yegian
+#9/29/17
+#divisors.py - finds all divisors of a number
 
+num = int(input('Enter a number: '))
+while i<num:
+    if num%i == 0:
+        div = i
+        print(div)
+    i = i+1
