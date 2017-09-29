e145227419a56cd0da4b1e1bacf0498585e54c37
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-29 17:02:15 -0400

    Updated from Brython Server: 9/29/2017 5:02:14 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -4,8 +4,9 @@
 
 num = int(input('Enter a number: '))
 i = 1
+total = 0
 while i<num:
     if num%i == 0:
         div = i
-        total = div
-        
+        total = total + div
+    i = i + 1
