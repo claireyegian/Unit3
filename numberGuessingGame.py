ba60ba6c83f99d434b5e11cccd64ed630d932b22
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-29 14:48:06 -0400

    Updated from Brython Server: 9/29/2017 2:48:05 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,16 @@
+#Claire Yegian
+#9/29/17
+#numberGuessingGame.py - asks user to guess number between 1 and 100
 
+from random import randint
+rnum = randint(1,100)
+gnum = int(input('Guess a number between 1 and 100: '))
+
+if gnum==rnum:
+    print('You got in on the first try!')
+elif:
+    while gnum!=rnum:
+        if gnum<rnum:
+            print(too low)
+        elif gnum>rnum:
+            print(too high)
