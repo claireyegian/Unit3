25af8b28a5ffe4595bb5c1704688986cf4fb3ae0
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-03 21:01:35 -0400

    Update warmUp8.py

--- warmUp8.py
+++ warmUp8.py
@@ -1,1 +1,11 @@
+#Claire Yegian
+#10/3/17
+#warmUp8.py - finds sum of all numbers less than 100,000 that are divisible by 3,10, and 2017
 
+num = 1
+total = 0
+while num<100000:
+    if num%3==0 and num%10==0 and num%17==00:
+        total += num
+    num += 1
+print(total)
