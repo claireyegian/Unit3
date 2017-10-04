9794dbb58a1c6b0ed64fc42cfd92a5909f2bb227
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-04 13:54:55 -0400

    Updated from Brython Server: 10/4/2017 1:54:54 PM

--- fri13.py
+++ fri13.py
@@ -2,22 +2,18 @@
 #10/3/17
 #fri13.py - finds next 10 friday the 13ths
 
-"""
-day1 = int(input('Enter your birth day: '))
-month1 = int(input('Enter the number of your birth month: '))
-year1 = int(input('Enter your birth year: '))
 day2 = int(date.today().day)
 month2 = int(date.today().month)
 year2 = int(date.today().year)
 
 weekday = weekday(year2,month2,day2)
-
-if weekday==3 and day2==13"""
-
-num1 = int(input('enter:' ))
-
-i=num1
-while 1<=100:
-    if num1%i==0:
-        print(num1)
-    i += 1
+fri13 = 0
+while fri13<=10:
+    if day2==13 and weekday==4:
+        print(weekday)
+    else:
+        if month2!=12:
+            month2 += 1
+        else:
+            month2 = 0
+            year2 = year2 + 1
