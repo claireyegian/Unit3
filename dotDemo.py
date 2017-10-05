ecbe6300fc91c1daac4c0bff822bc7fb67efef2a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-05 14:31:09 -0400

    Update dotDemo.py

--- dotDemo.py
+++ dotDemo.py
@@ -1,1 +1,14 @@
+#Claire Yegian
+#10/5/17
+#dotDemo.py - Making some dots
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+
+dot = CircleAsset(20,LineStyle(1,red),red)
+
+for j in range(10):
+    for i in range(20):
+        Sprite(dot,(20,20))
+App().run()
