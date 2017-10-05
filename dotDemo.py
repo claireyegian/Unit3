227fd360e6506b504f8ed5408d1cbefffaaac2bf
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-05 14:33:22 -0400

    Update dotDemo.py

--- dotDemo.py
+++ dotDemo.py
@@ -8,7 +8,8 @@ red = Color(0xFF0000,1)
 
 dot = CircleAsset(20,LineStyle(1,red),red)
 
-for j in range(10):
-    for i in range(20):
-        Sprite(dot,(20,20))
+for j in range(10): #prints the row 10 times
+    for i in range(20): #prints one row of dots
+        Sprite(dot,(20 + 50 * i,20 + 50 * j))
 App().run()
+
