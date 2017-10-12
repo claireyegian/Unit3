f5876ee99e161f1e4d82116b8d2459641a7a1c04
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-12 14:19:01 -0400

    Updated from Brython Server: 10/12/2017 2:19:01 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -5,23 +5,25 @@
 num = int(input('Enter the number of cents you need to give in change: '))
 
 quarters = 0
-While num>=25:
+while num>=25:
     num = num - 25
     quarters = quarters + 1
+print('Quarters: ', quarters)
 
 dimes = 0
-While num>=10:
+while num>=10:
     num = num - 10
     dimes = dimes + 1
+print('Dimes: ', dimes)
 
 nickels = 0
-While num>=5:
+while num>=5:
     num = num - 5
     nickels = nickels + 1
+print('Nickels: ', nickels)
 
 pennies = 0
-While num>=1:
+while num>=1:
     num = num - 1
     pennies = pennies + 1
-
-print('You have', quarters, 'quarters, ', dimes 'dimes, ', nickels, 'nickels, and ', pennies, 'pennies')
+print('Pennies: ', pennies)
