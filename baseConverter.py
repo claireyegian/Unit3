2c5dde8d9ea43aeb50798e0b80d9e841756f8003
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-12 14:51:06 -0400

    Updated from Brython Server: 10/12/2017 2:51:06 PM

--- baseConverter.py
+++ baseConverter.py
@@ -2,3 +2,12 @@
 #10/12/17
 #baseConverter.py - changes base-10 to any other base between 2 and 16
 
+base10 = int(input('Enter a base-10 number: '))
+base = int(input('What base whould you like to convert to? '))
+
+i = 1
+while base10<=(base**i):
+    digits = i
+    i = i + 1
+
+print(digits)
