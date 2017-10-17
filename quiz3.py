a8e967de5ae8c4ad747e9cde2119504900a59270
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-17 14:19:46 -0400

    Updated from Brython Server: 10/17/2017 2:19:44 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,15 @@
+#Claire Yegian
+#10/17/17
+#quiz3.py - idek what this thing does
 
+for i in range(-5,6,1):
+    print(i)
+
+i = 18
+while i <=32:
+    print (i)
+    i += 2
+
+total = 0
+for i in range(13,332,2):
+    total += i
